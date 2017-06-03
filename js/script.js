b908e9139efb5c31282e65750a261f95fe2227d8
brent-edwards-online <brent@brentedwardsonline.com>
2017-06-03 21:13:23 +1000

    no message

--- js/script.js
+++ js/script.js
@@ -39,4 +39,48 @@ $(document).ready(() => {
   $('#btnClear').on('click', () => {
     
   });
+
+  $('#btn1').css({ marginTop: '20px', marginBottom: '20px' });
+  $('p.para1').css({color:'red', backgroundColor: 'grey'});
+  $('p.para2').addClass('myclass');
+
+  $('#btn1').on('click', () => {
+    $('p.para2').toggleClass('myclass');
+  });
+
+  $('div#mydiv').text('Waszup?');
+  $('div#myhtmldiv').html('<h3>Waszup?</h3>');
+
+  $('#btn2').on('click', () => {
+    $('ul#unorderedList').append('<li>Append Item</li>');
+    $('ul#unorderedList').prepend('<li>Prepend Item</li>');
+
+    $('p.para1').appendTo('p.para2');
+
+    $('ul#unorderedList').before('<h5>Before</h5>')
+    $('ul#unorderedList').after('<h5>After</h5>')
+  });
+
+  let myArray = ['brent','joe','alex','jose']
+
+  $.each(myArray, (idx, val) => {
+      $('ul#unorderedList').append('<li>' + val + '</li>');
+  });
+  
+  $('#btnFadeOut').click(() => {
+    $('#box').fadeOut(5000, () => {
+      $('#btnFadeOut').text('Its gone');
+    });
+  });
+
+  $('#btnFadeIn').click(() => {
+    $('#box').fadeIn(5000,() => {
+      $('#btnFadeOut').text('Its back...');
+    });
+  });
+
+  $('#btnFadeToggle').click(() => {
+    $('#box').fadeToggle(3000);
+  });
+
 });
